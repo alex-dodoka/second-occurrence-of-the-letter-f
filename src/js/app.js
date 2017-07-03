a06b483cd71c8d6a54c0a59124b0be194c06e2f9
alex-dodoka <neksus.92@mail.ru>
2017-07-04 01:45:14 +0300

    Finding the second occurrence of a letter.

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,24 @@
+'use strict';
+const arrOfLetters = [];
+
 function findF() {
-    let string = $('#some-string').val();
+    let string = $('#some-string').val().split('');
 
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === "f") {
+            arrOfLetters.push(i);
+        }
+    }
 
+    if (arrOfLetters.length === 1) {
+        printHtml("-1")
+    } else if (arrOfLetters.length === 0) {
+        printHtml("-2")
+    } else {
+        printHtml(arrOfLetters[1]);
+    }
 
 }
-
 
 function printHtml(res) {
     $('#space-for-print').html(res)
